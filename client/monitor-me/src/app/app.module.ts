961fe4f5e5b6531dd6102824e90901db8069fc86
sowakarol <karosowa@student.agh.edu.pl>
2017-09-20 23:08:23 +0200

    Added self-registration-form component

--- client/monitor-me/src/app/app.module.ts
+++ client/monitor-me/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SelfRegistrationFormComponent } from './self-registration-form/self-registration-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SelfRegistrationFormComponent
   ],
   imports: [
     BrowserModule
---
+++ client/monitor-me/src/app/self-registration-form/self-registration-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-self-registration-form',
+  templateUrl: './self-registration-form.component.html',
+  styleUrls: ['./self-registration-form.component.scss']
+})
+export class SelfRegistrationFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
